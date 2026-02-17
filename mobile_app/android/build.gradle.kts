817f3fb63d5be77130acebbc9362e36bd0c27170
DirectorM <directorm@DirectorMs-MacBook-Pro.local>
2026-02-17 19:56:24 +0100

    FIX: Hard Revert to Legacy Stack (Gradle 7.6, AGP 7.4.2, Kotlin 1.8.22)

--- mobile_app/android/build.gradle.kts
+++ mobile_app/android/build.gradle.kts
@@ -1,3 +1,15 @@
+buildscript {
+    val kotlin_version by extra("1.8.22")
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version")
+    }
+}
+
 allprojects {
     repositories {
         google()
@@ -18,7 +30,7 @@ subprojects {
 subprojects {
     project.evaluationDependsOn(":app")
     
-    // Workaround for flutter_paystack not having namespace in Gradle 8
+    // Workaround for flutter_paystack not having namespace in Gradle 8 (Keep for safety even if 7.4.2 usually doesn't need it as strict)
     if (project.name == "flutter_paystack") {
         project.plugins.withId("com.android.library") {
             val android = project.extensions.getByType(com.android.build.gradle.LibraryExtension::class.java)
